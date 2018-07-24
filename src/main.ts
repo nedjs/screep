1d3584c9877850457d40193db8df8e8679c48b8b
nstuart <>
2018-07-24 15:36:45 -0700

    shit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ErrorMapper } from "utils/ErrorMapper";
 export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current game tick is ${Game.time}`);
 
+  Game.spawns.
+
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
     if (!(name in Game.creeps)) {
